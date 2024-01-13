3cd19a3d0fd59fe9c45929b37549468e00274f63
willematack <44038988+willematack@users.noreply.github.com>
2024-01-12 20:44:02 -0500

    Update Memory.py

--- Memory.py
+++ Memory.py
@@ -7,26 +7,16 @@ import torch
 
 class Buffer():
 
-    def __init__(self, TRANSITIONDIRECTORY: str, max_size = 500, save_frequency = 100):
+    def __init__(self, iteration_size, time_steps):
 
-        self.mem_size = int(max_size)
-        self.file_path = TRANSITIONDIRECTORY
-        self.save_frequency  = save_frequency
-
+        self.mem_size = int(iteration_size*time_steps)
         self.mem_cntr = 0
+
         self.state_memory = torch.zeros(size = (self.mem_size, 2, 60, 60))
         self.new_state_memory = torch.zeros(size = (self.mem_size, 2, 60, 60))
         self.action_memory = torch.zeros(size = (self.mem_size, 2))
 
         self.mem_cntr_initial = self.mem_cntr
-
-    def initiate_storage(self):
-        '''Reset the transition tuples (necessary at the beginning of generation)
-        '''
-        
-        torch.save(torch.zeros(size = (1,2,60,60)), self.file_path+'/state.pt')
-        torch.save(torch.zeros(size = (1,2,60,60)), self.file_path+'/state_.pt')
-        torch.save(torch.zeros(size = (1,2)), self.file_path+'/state.pt')
 
     def store_transition(self, state, action, new_state):
         '''Store a state, action, and new state to the buffer
@@ -34,7 +24,8 @@ class Buffer():
 
         index = self.mem_cntr % self.mem_size
         self.state_memory[index] = state.detach()
-        self.new_state_memory[index] = new_state.detach()
         self.action_memory[index] = action.detach()
+        self.new_state_memory[index] = new_state.detach()
+        
 
         self.mem_cntr += 1
